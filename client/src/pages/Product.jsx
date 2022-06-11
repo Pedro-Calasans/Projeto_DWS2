9a6531c53c0829f79bf541d3002e80bbfd9e32f2
DemetriosP <pantaleao15@hotmail.com>
2022-06-11 17:51:23 -0300

    Alteração no carrinho
    Agora é possível remover produtos do carrinho, aumentar a quantidade dos produtos no carrinho e diminuir a quantidade dos produtos no carrinho
    
    Um botão para limpar os itens do carrinho foi adicionado
    
    É possível adicionar o produto no carrinho através do símbolo do carrinho nos produtos
    
    O valor do frete e o desconto é calculado de acordo com o valor total dos produtos

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,184 +1,187 @@
-import { Add, Remove } from "@material-ui/icons";
-import React, { useEffect } from "react";
+import {Add, Remove} from "@material-ui/icons";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
-import { mobile } from "../responsive";
-import { useLocation } from "react-router-dom";
-import { publicRequest } from "../requestMethods";
-import { useState } from "react";
-import { addProduct } from "../redux/cartRedux";
-import { useDispatch } from "react-redux";
+import {mobile} from "../responsive";
+import {useLocation} from "react-router-dom";
+import {publicRequest} from "../requestMethods";
+import {addProduct} from "../redux/cartRedux";
+import {useDispatch} from "react-redux";
 
 
 const Container = styled.div``;
 
 const Wrapper = styled.div`
-	padding: 50px;
-	display: flex;
-	${mobile({ padding: "10px", flexDirection: "column" })}
+  padding: 50px;
+  display: flex;
+  ${mobile({padding: "10px", flexDirection: "column"})}
 `;
 
 const ImageContainer = styled.div`
-	flex: 1;
+  flex: 1;
 `;
 
 const Image = styled.img`
-	width: 100%;
-	height: 90vh;
-	object-fit: cover;
-	${mobile({ height: "40vh" })}
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+  ${mobile({height: "40vh"})}
 `;
 
 const InfoContainer = styled.div`
-	flex: 1;
-	padding: 0px 50px;
-	${mobile({ padding: "10px" })}
+  flex: 1;
+  padding: 0px 50px;
+  ${mobile({padding: "10px"})}
 `;
 
 const Title = styled.h1`
-	font-weight: 200;
+  font-weight: 200;
 `;
 
 const Description = styled.p`
-	margin: 20px 0px;
+  margin: 20px 0px;
 `;
 
 const Price = styled.span`
-	font-weight: 100;
-	font-size: 40px;
+  font-weight: 100;
+  font-size: 40px;
 `;
 
 const FilterContainer = styled.div`
-	width: 50%;
-	margin: 30px 0px;
-	display: flex;
-	justify-content: space-between;
-	${mobile({ width: "100%" })}
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  ${mobile({width: "100%"})}
 `;
 
 const Filter = styled.div`
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 `;
 
 const FilterTitle = styled.span`
-	font-size: 20px;
-	font-weight: 200;
+  font-size: 20px;
+  font-weight: 200;
 `;
 
 const FilterColor = styled.div`
-	width: 20px;
-	height: 20px;
-	border-radius: 50%;
-	background-color: ${(props) => props.color};
-	margin: 0px 5px;
-	cursor: pointer;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
 `;
 
 const FilterSize = styled.select`
-	margin-left: 10px;
-	padding: 5px;
+  margin-left: 10px;
+  padding: 5px;
 `;
 
 const FilterSizeOption = styled.option``;
 
 const AddContainer = styled.div`
-	width: 50%;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	${mobile({ width: "100%" })}
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({width: "100%"})}
 `;
 
 const AmountContainer = styled.div`
-	display: flex;
-	align-items: center;
-	font-weight: 700;
+  display: flex;
+  align-items: center;
+  font-weight: 700;
 `;
 
 const Amount = styled.span`
-	width: 30px;
-	height: 30px;
-	border-radius: 10px;
-	border: 1px solid green;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	margin: 0px 5px;
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid green;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
 `;
 
 const Button = styled.button`
-	padding: 15px;
-	border: 2px solid green;
-	background-color: white;
-	cursor: pointer;
-	font-weight: 500;
+  padding: 15px;
+  border: 2px solid green;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
 
-	&:hover {
-		background-color: #f1f1f1;
-	}
+  &:hover {
+    background-color: #f1f1f1;
+  }
 `;
 
 const Product = () => {
-	const location = useLocation();
-	const id = location.pathname.split("/")[2];
-	const [product, setProduct] = useState({});
-	const [quantity, setQuantity] = useState(1);
-	const dispatch = useDispatch();
+    const location = useLocation();
+    const id = location.pathname.split("/")[2];
+    const [product, setProduct] = useState({});
+    const [quantity, setQuantity] = useState(1);
+    const dispatch = useDispatch();
 
-	useEffect(() => {
-		const getProduct = async () => {
-		  try {
-			const res = await publicRequest.get("/products/find/" + id);
-			setProduct(res.data);
-		  } catch {}
-		};
-		getProduct();
-	  }, [id]);
+    useEffect(() => {
+        const getProduct = async () => {
+            try {
+                const res = await publicRequest.get("/products/find/" + id);
+                setProduct(res.data);
+            } catch {
+            }
+        };
+        getProduct();
+    }, [id]);
 
-	const handleQuantity = (type) =>{
-		if(type ===  "dec" && quantity>1){
-			setQuantity(quantity-1);
-		} if(type === "inc"){
-			setQuantity(quantity+1);
-		}
-	}
+    const handleQuantity = (type) => {
+        if (type === "dec" && quantity > 1) {
+            setQuantity(quantity - 1);
+        }
+        if (type === "inc") {
+            setQuantity(quantity + 1);
+        }
+    }
 
-	const handleClick = ()=>{
-		dispatch(addProduct({...product,quantity}));		
-	}
+    console.log(product)
 
-	return (
-		<Container>
-			<Navbar />
-			<Announcement />
-			<Wrapper>
-				<ImageContainer>
-					<Image src={product.img} />
-				</ImageContainer>
-				<InfoContainer>
-					<Title>{product.tittle}</Title>
-					<Description>{product.desc}</Description>
-					<Price> R$ {product.price}</Price>
-					<FilterContainer>
-					</FilterContainer>
-					<AddContainer>
-						<AmountContainer>
-						<Remove onClick={() => handleQuantity("dec")} />
-						<Amount>{quantity}</Amount>
-						<Add onClick={() => handleQuantity("inc")} />
-						</AmountContainer>
-						<Button onClick={() => handleClick()}>Adicionar ao Carrinho</Button>
-					</AddContainer>
-				</InfoContainer>
-			</Wrapper>
-			<Newsletter />
-			<Footer />
-		</Container>
-	);
+    const handleClick = () => {
+        dispatch(addProduct({...product, quantity}));
+    }
+
+    return (
+        <Container>
+            <Navbar/>
+            <Announcement/>
+            <Wrapper>
+                <ImageContainer>
+                    <Image src={product.img}/>
+                </ImageContainer>
+                <InfoContainer>
+                    <Title>{product.tittle}</Title>
+                    <Description>{product.desc}</Description>
+                    <Price> R$ {product.price}</Price>
+                    <FilterContainer>
+                    </FilterContainer>
+                    <AddContainer>
+                        <AmountContainer>
+                            <Remove onClick={() => handleQuantity("dec")}/>
+                            <Amount>{quantity}</Amount>
+                            <Add onClick={() => handleQuantity("inc")}/>
+                        </AmountContainer>
+                        <Button onClick={() => handleClick()}>Adicionar ao Carrinho</Button>
+                    </AddContainer>
+                </InfoContainer>
+            </Wrapper>
+            <Newsletter/>
+            <Footer/>
+        </Container>
+    );
 };
 
 export default Product;
